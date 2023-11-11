465c4da031e184d3d3ca62c3cbde5c31f0ba4f0c
prcryx <me.prc.fo@gmail.com>
2023-11-11 16:16:27 +0530

    add new custom exceptions

---
+++ internal/common/exception/custom_exception_msg.go
@@ -0,0 +1,10 @@
+package exception
+
+const (
+	InternalServerErrorMsg          = "internal server error"
+	InvalidReqBodyMsg               = "invalidrequest body"
+	SigningErrorMsg                 = "signing error"
+	TooShortPasswordMsg             = "password is too short"
+	RequireSpecialCharInPasswordMsg = "password must contain at least one special character"
+	RequiredNumInPasswordMsg        = "password must contain at least one numeric character"
+)
--- internal/common/exception/custom_exceptions.go
+++ internal/common/exception/custom_exceptions.go
@@ -8,13 +8,23 @@ func EmptyError() AppException {
 	return newAppException(0, "")
 }
 func InternalServerError(debugMsg ...string) AppException {
-	return newAppException(http.StatusInternalServerError, "internal server error", debugMsg...)
+	return newAppException(http.StatusInternalServerError, InternalServerErrorMsg, debugMsg...)
 }
 
 func InvalidBodyRequestException() AppException {
-	return newAppException(http.StatusBadRequest, "invalid body request")
+	return newAppException(http.StatusBadRequest, InvalidReqBodyMsg)
 }
 
 func UnacceptedSigningError() AppException {
-	return newAppException(http.StatusForbidden, "signing error")
+	return newAppException(http.StatusForbidden, SigningErrorMsg)
+}
+
+func TooShortPasswordException() AppException {
+	return newAppException(http.StatusBadRequest, TooShortPasswordMsg)
+}
+func SpecialCharRequiredInPasswordException() AppException {
+	return newAppException(http.StatusBadRequest, RequireSpecialCharInPasswordMsg)
+}
+func NumRequiredInPasswordException() AppException {
+	return newAppException(http.StatusBadRequest, RequiredNumInPasswordMsg)
 }
